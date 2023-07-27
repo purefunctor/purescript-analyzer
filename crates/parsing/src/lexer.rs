425321a469a6ac74dabec1635ba0042d8a7635f7
Justin Garcia <purefunctor@gmail.com>
2023-07-27 14:55:53 +0800

    More links in the documentation

--- crates/parsing/src/lexer.rs
+++ crates/parsing/src/lexer.rs
@@ -1,5 +1,11 @@
 //! Text-based lexer into a sequence of [`SyntaxKind`]s.
-//! Based of off https://github.com/purescript/purescript/blob/master/src/Language/PureScript/CST/Lexer.hs
+//! 
+//! Tokens are based off of the compiler's [`lexer`], while the lexer
+//! core borrows from [`rustc_lexer`] and [`rust-analyzer`].
+//! 
+//! [`lexer`]: https://github.com/purescript/purescript/blob/master/src/Language/PureScript/CST/Lexer.hs
+//! [`rustc_lexer`]: https://doc.rust-lang.org/stable/nightly-rustc/rustc_lexer/
+//! [`rust-analyzer`]: https://github.com/rust-lang/rust-analyzer/
 
 use std::{ops::Range, str::Chars};
 
