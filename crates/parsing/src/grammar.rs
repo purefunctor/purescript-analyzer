39df9ca35e56c3abd84d7f71ba41fc6b81b70a43
Justin Garcia <purefunctor@gmail.com>
2023-07-31 01:24:58 +0800

    Extract prefix parser from qualified_name

--- crates/parsing/src/grammar.rs
+++ crates/parsing/src/grammar.rs
@@ -17,22 +17,25 @@ pub fn expression_atom(parser: &mut Parser) {
     }
 }
 
-pub fn qualified_name(parser: &mut Parser) {
-    let mut qualified = parser.start();
-
+pub fn qualified_prefix(parser: &mut Parser) {
     let mut prefix = parser.start();
     loop {
         if parser.at(SyntaxKind::Upper) && parser.nth_at(1, SyntaxKind::Period) {
             let mut name = parser.start();
             parser.consume();
             name.end(parser, SyntaxKind::NameRef);
-
             parser.consume();
         } else {
             break;
         }
     }
     prefix.end(parser, SyntaxKind::QualifiedPrefix);
+}
+
+pub fn qualified_name(parser: &mut Parser) {
+    let mut qualified = parser.start();
+
+    qualified_prefix(parser);
 
     match parser.current() {
         SyntaxKind::Upper | SyntaxKind::Lower | SyntaxKind::AsKw => {
