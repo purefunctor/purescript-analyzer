dd6ac5b9b5fe2f3e0111f7643be049640a5b88a7
purefunctor <purefunctor@gmail.com>
2023-08-31 01:54:14 +0800

    Add annotation and siblings

--- crates/analyzer/src/lower/surface.rs
+++ crates/analyzer/src/lower/surface.rs
@@ -1,11 +1,26 @@
 //! The core AST used for semantic analysis.
 
+use std::sync::Arc;
+
 use la_arena::{Arena, Idx};
+use syntax::ast;
+
+use crate::id::{AstId, InFile};
 
 #[derive(Debug, PartialEq, Eq, Hash)]
 pub struct ValueDeclarationData {
     pub expr_arena: Arena<Expr>,
     pub expr_id: Idx<Expr>,
+    /// The type annotation for this value declaration, if it exists.
+    pub annotation: Option<AstId<ast::AnnotationDeclaration>>,
+    /// PureScript supports "equational" style declarations.
+    ///
+    /// ```haskell
+    /// isZero :: Int ->
+    /// isZero 0 = true
+    /// isZero _ = false
+    /// ```
+    pub siblings: Arc<[InFile<AstId<ast::ValueDeclaration>>]>,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
