b7e7dd4a9206dcda4b565fb9b0a727abcd185062
purefunctor <purefunctor@gmail.com>
2023-09-07 21:06:13 +0800

    Implement NameRef and Qualified

--- crates/analyzer/src/names.rs
+++ crates/analyzer/src/names.rs
@@ -47,3 +47,36 @@ impl From<ast::QualifiedPrefix> for ModuleName {
         ModuleName { inner }
     }
 }
+
+/// e.g. `Just`, `fromMaybe`
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct NameRef {
+    inner: Arc<str>,
+}
+
+impl TryFrom<ast::NameRef> for NameRef {
+    type Error = &'static str;
+
+    fn try_from(value: ast::NameRef) -> Result<Self, Self::Error> {
+        let name_ref = value.as_str().ok_or("Cannot convert NameRef")?.to_string();
+        Ok(NameRef { inner: name_ref.into() })
+    }
+}
+
+/// e.g. `M.Just`, `M.fromMaybe`
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct Qualified<N> {
+    prefix: Option<ModuleName>,
+    value: N,
+}
+
+impl TryFrom<ast::QualifiedName> for Qualified<NameRef> {
+    type Error = &'static str;
+
+    fn try_from(value: ast::QualifiedName) -> Result<Self, Self::Error> {
+        let prefix = value.prefix().map(ModuleName::from);
+        let value =
+            value.name_ref().ok_or("QualifiedName has no NameRef").and_then(NameRef::try_from)?;
+        Ok(Qualified { prefix, value })
+    }
+}
