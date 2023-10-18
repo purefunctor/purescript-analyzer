19df2231a64220e338e9aebb6504f0ad5d482ae8
purefunctor <purefunctor@pm.me>
2023-10-19 00:43:22 +0800

    Add documentation for trees

--- crates/analyzer/src/infer/trees.rs
+++ crates/analyzer/src/infer/trees.rs
@@ -1,3 +1,13 @@
+//! ASTs for type inference.
+//! 
+//! Unlike the definition from [`surface`], [`Type`] makes use of [`salsa`]'s
+//! interning mechanism rather than an [`Arena`] that gets passed around. The
+//! main reason for this difference is the fact that type inference operates
+//! at a global context. Using [`Arena`]s would be impractical, since it would
+//! mean that types cannot be shared across two things that need inference.
+//! 
+//! [`Arena`]: la_arena::Arena
+//! [`surface`]: crate::surface
 use syntax::ast;
 
 use crate::id::{AstId, InFile};
