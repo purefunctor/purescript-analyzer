df3428aa8479478e26f436ca1177ff53d20affdc
Justin Garcia <purefunctor@gmail.com>
2023-10-21 22:41:35 +0800

    Manually derive instances for AstId

--- crates/analyzer/src/id.rs
+++ crates/analyzer/src/id.rs
@@ -1,6 +1,6 @@
 //! IDs used as query keys.
 
-use std::marker::PhantomData;
+use std::{hash::Hash, marker::PhantomData};
 
 use files::FileId;
 use la_arena::Idx;
@@ -10,7 +10,7 @@ use syntax::{PureScript, SyntaxNodePtr};
 /// See documentation for [`PositionalMap`].
 ///
 /// [`PositionalMap`]: crate::resolver::PositionalMap
-#[derive(Debug, PartialEq, Eq, Hash)]
+#[derive(Debug)]
 pub struct AstId<N: AstNode<Language = PureScript>> {
     pub(crate) raw: Idx<SyntaxNodePtr>,
     _marker: PhantomData<fn() -> N>,
@@ -24,6 +24,32 @@ impl<N: AstNode<Language = PureScript>> Clone for AstId<N> {
 
 impl<N: AstNode<Language = PureScript>> Copy for AstId<N> {}
 
+impl<N: AstNode<Language = PureScript>> PartialEq for AstId<N> {
+    fn eq(&self, other: &Self) -> bool {
+        self.raw == other.raw
+    }
+}
+
+impl<N: AstNode<Language = PureScript>> Eq for AstId<N> {}
+
+impl<N: AstNode<Language = PureScript>> PartialOrd for AstId<N> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<N: AstNode<Language = PureScript>> Ord for AstId<N> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.raw.cmp(&other.raw)
+    }
+}
+
+impl<N: AstNode<Language = PureScript>> Hash for AstId<N> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.raw.hash(state);
+    }
+}
+
 impl<N: AstNode<Language = PureScript>> AstId<N> {
     pub(crate) fn new(raw: Idx<SyntaxNodePtr>) -> AstId<N> {
         AstId { raw, _marker: PhantomData }
@@ -34,7 +60,7 @@ impl<N: AstNode<Language = PureScript>> AstId<N> {
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct InFile<T> {
     pub(crate) file_id: FileId,
     pub(crate) value: T,
