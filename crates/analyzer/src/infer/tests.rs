76710aba5dfe6bec16f427dad19e820c1428bc53
Justin Garcia <purefunctor@gmail.com>
2023-12-03 02:53:25 +0800

    Simplify value tests

--- crates/analyzer/src/infer/tests.rs
+++ crates/analyzer/src/infer/tests.rs
@@ -1,12 +1,8 @@
 use std::{fmt::Write, fs, sync::Arc};
 
 use files::{ChangedFile, Files};
-use indexmap::IndexSet;
 use insta::glob;
-use rowan::ast::AstNode;
 use salsa::Durability;
-use smol_str::SmolStr;
-use syntax::ast;
 
 use crate::{infer, InferDatabase, ResolverDatabase, RootDatabase, SourceDatabase};
 
@@ -24,30 +20,13 @@ fn expect_infer_value(source: &str) {
     db.set_file_paths_with_durability(files.iter().collect(), Durability::HIGH);
 
     let file_id = files.file_id("./Main.purs".into()).unwrap();
-    let node = db.parse_file(file_id);
-    let declarations = ast::Source::<ast::Module>::cast(node)
-        .and_then(|source| Some(source.child()?.body()?.declarations()?.children()))
-        .unwrap();
-
-    let names: IndexSet<SmolStr> = declarations
-        .into_iter()
-        .filter_map(|declaration| {
-            if let ast::Declaration::ValueEquationDeclaration(equation) = declaration {
-                equation.name()?.as_str()
-            } else {
-                None
-            }
-        })
-        .collect();
-
     let nominal_map = db.nominal_map(file_id);
     let pp = infer::PrettyPrinter::new(&db);
 
     let mut results = String::new();
-    for name in names {
-        let name = name.as_ref();
-        let id = nominal_map.value_group_id(name).unwrap();
-        let (ty, _) = db.infer_value(id);
+    for (value_group_id, value_group_data) in nominal_map.value_groups() {
+        let name = value_group_data.name.as_ref();
+        let (ty, _) = db.infer_value(value_group_id);
         writeln!(&mut results, "{} :: {}", name, pp.ty(ty).pretty(80)).unwrap();
     }
 
