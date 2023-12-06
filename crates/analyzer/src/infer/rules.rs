83942280b634061404ea139b698e193372824713
purefunctor <purefunctor@pm.me>
2023-12-07 00:08:15 +0800

    Implement initial constraint solver

--- crates/analyzer/src/infer/rules.rs
+++ crates/analyzer/src/infer/rules.rs
@@ -32,6 +32,14 @@ struct UnifyContext<'env, 'state> {
     infer_state: &'state mut InferState,
 }
 
+struct SolveContext<'env, 'state> {
+    db: &'env dyn InferDatabase,
+    infer_state: &'state mut InferState,
+
+    unification_solved: FxHashMap<Unification, TypeId>,
+    unification_deferred: Vec<(Unification, Unification)>,
+}
+
 struct InferBindingGroupContext<'env, 'state> {
     db: &'env dyn InferDatabase,
     id: InFile<BindingGroupId>,
@@ -75,6 +83,17 @@ impl<'env, 'state> UnifyContext<'env, 'state> {
         infer_state: &'state mut InferState,
     ) -> UnifyContext<'env, 'state> {
         UnifyContext { db, infer_state }
+    }
+}
+
+impl<'env, 'state> SolveContext<'env, 'state> {
+    fn new(
+        db: &'env dyn InferDatabase,
+        infer_state: &'state mut InferState,
+    ) -> SolveContext<'env, 'state> {
+        let unification_solved = FxHashMap::default();
+        let unification_deferred = Vec::default();
+        SolveContext { db, infer_state, unification_solved, unification_deferred }
     }
 }
 
@@ -156,6 +175,54 @@ impl<'env, 'state> UnifyContext<'env, 'state> {
             (_, _) => {
                 unimplemented!("Oh No!");
             }
+        }
+    }
+}
+
+impl<'env, 'state> SolveContext<'env, 'state> {
+    fn step(&mut self) {
+        while let Some(constraint) = self.infer_state.constraints.pop() {
+            match constraint {
+                Constraint::UnifyDeep(x_u, y_u) => {
+                    let x_s = self.unification_solved.get(&x_u).copied();
+                    let y_s = self.unification_solved.get(&y_u).copied();
+                    match (x_s, y_s) {
+                        (Some(x_t), Some(y_t)) => {
+                            UnifyContext::new(self.db, self.infer_state).unify(x_t, y_t);
+                        }
+                        (Some(x_t), None) => {
+                            self.unification_solved.insert(y_u, x_t);
+                        }
+                        (None, Some(y_t)) => {
+                            self.unification_solved.insert(x_u, y_t);
+                        }
+                        (None, None) => {
+                            self.unification_deferred.push((x_u, y_u));
+                        }
+                    }
+                }
+                Constraint::UnifySolve(x_u, y_t) => {
+                    self.unification_solved.insert(x_u, y_t);
+                }
+            }
+        }
+
+        self.unification_deferred.retain(|(x_u, y_u)| {
+            let x_s = self.unification_solved.get(x_u).copied();
+            let y_s = self.unification_solved.get(y_u).copied();
+
+            if x_s.is_none() && y_s.is_none() {
+                return true;
+            }
+
+            self.infer_state.constraints.push(Constraint::UnifyDeep(*x_u, *y_u));
+            false
+        });
+    }
+
+    fn solve(&mut self) {
+        while !self.infer_state.constraints.is_empty() {
+            self.step();
         }
     }
 }
@@ -458,6 +525,11 @@ pub(crate) fn infer_binding_group_query(
 
     let binding_groups = db.binding_groups(id.file_id);
     context.infer(&binding_groups[id.value]);
+
+    let mut solver = SolveContext::new(db, context.infer_state);
+    solver.solve();
+
+    dbg!(solver.unification_solved);
 
     Arc::new(InferBindingGroup::new(context.of_value_group, infer_state.constraints))
 }
