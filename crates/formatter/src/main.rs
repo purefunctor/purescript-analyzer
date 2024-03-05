4da6f57344733c89eed43582609bd46625fb8319
Edvard Thörnros <edvard.thornros@gmail.com>
2024-03-05 23:43:46 +0100

    Parse a bunch of files instead

--- crates/formatter/src/main.rs
+++ crates/formatter/src/main.rs
@@ -3,7 +3,6 @@
 use std::fs;
 
 use gumdrop::Options;
-use syntax::SyntaxNode;
 
 #[derive(Options)]
 struct Args {
@@ -11,24 +10,25 @@ struct Args {
     help: bool,
 
     #[options(required, free, help = "the source file to parse")]
-    source_file: String,
+    source_files: Vec<String>,
 }
 
 pub fn main() {
     let args = Args::parse_args_default_or_exit();
-    let source =
-        fs::read_to_string(args.source_file).expect("Should have been able to read the file");
+    for file in args.source_files.iter() {
+        let source =
+            fs::read_to_string(file).expect("Should have been able to read the file");
 
-    let (node, errors) = parsing::parse_module(&source);
-
-    render(node, 0);
-
-    println!("{:?}", errors);
-}
-
-fn render(node: SyntaxNode, indentation: usize) {
-    println!("{:indentation$}{:?}", "", node.kind(), indentation = indentation);
-    for child in node.children() {
-        render(child, indentation + 1);
+        let (_node, errors) = parsing::parse_module(&source);
+        // render(node, 0);
+        println!("{}:{:?}", file, errors);
     }
+    println!("{}", args.source_files.len());
 }
+
+// fn render(node: SyntaxNode, indentation: usize) {
+//     println!("{:indentation$}{:?}", "", node.kind(), indentation = indentation);
+//     for child in node.children() {
+//         render(child, indentation + 1);
+//     }
+// }
