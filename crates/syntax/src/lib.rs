ac6b99f9956d27a7e2820db0dc043b89265960d2
Justin Garcia <purefunctor@gmail.com>
2024-12-03 03:41:27 +0800

    chore(syntax): remove unused code

--- crates/syntax/src/lib.rs
+++ crates/syntax/src/lib.rs
@@ -1,18 +1,3 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
-
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[allow(non_camel_case_types)]
 #[repr(u16)]
