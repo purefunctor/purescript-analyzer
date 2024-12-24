a6164983b95df0b9988ecb7281b3baae0ef874cf
Justin Garcia <purefunctor@gmail.com>
2024-12-24 12:38:10 +0800

    Add tests for recovery

--- crates/parsing/tests/parser/ModuleImportError.purs
+++ crates/parsing/tests/parser/ModuleImportError.purs
@@ -2,3 +2,9 @@ module ModuleImportError where
 
 import prelude
 import Data.List
+import Data.List (List(cons, nil))
+import Data.List (List(Cons, ..))
+import Data.List (List(.., Cons))
+import Data.List (123)
+import Data.List s Data.List
+import Recovered.Import
