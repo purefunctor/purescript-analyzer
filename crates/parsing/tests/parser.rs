8c52fdcfbdc602c4c038ea99d073edae7dc0f429
Justin Garcia <purefunctor@gmail.com>
2024-12-28 02:24:17 +0800

    Make it easier to add parser tests

--- crates/parsing/tests/parser.rs
+++ crates/parsing/tests/parser.rs
@@ -1,48 +1,8 @@
-macro_rules! parser_tests {
-    ($($name:ident => $source:expr),+ $(,)?) => {
-        $(
-            #[test]
-            fn $name() {
-                let lexed = lexing::lex($source);
-                let tokens = lexing::layout(&lexed);
-                let (node, errors) = parsing::parse(&lexed, &tokens);
-                insta::assert_debug_snapshot!((node, errors));
-            }
-        )+
-    };
-}
+use test_each_file::test_each_file;
 
-parser_tests!(
-    binder => include_str!("parser/Binder.purs"),
-    comment_pre_post => include_str!("parser/CommentPrePost.purs"),
-    expression => include_str!("parser/Expression.purs"),
-    lexer_error_inserted => include_str!("parser/LexerErrorInserted.purs"),
-    module_export => include_str!("parser/ModuleExport.purs"),
-    module_export_class => include_str!("parser/ModuleExportClass.purs"),
-    module_export_class_error_end => include_str!("parser/ModuleExportClassErrorEnd.purs"),
-    module_export_comma_error => include_str!("parser/ModuleExportCommaError.purs"),
-    module_export_data => include_str!("parser/ModuleExportData.purs"),
-    module_export_data_comma_error => include_str!("parser/ModuleExportDataCommaError.purs"),
-    module_export_data_item_after_double_period => include_str!("parser/ModuleExportDataItemAfterDoublePeriod.purs"),
-    module_export_data_item_before_double_period => include_str!("parser/ModuleExportDataItemBeforeDoublePeriod.purs"),
-    module_export_data_item_empty => include_str!("parser/ModuleExportDataItemEmpty.purs"),
-    module_export_data_item_invalid => include_str!("parser/ModuleExportDataItemInvalid.purs"),
-    module_export_data_item_recovery => include_str!("parser/ModuleExportDataItemRecovery.purs"),
-    module_export_empty => include_str!("parser/ModuleExportEmpty.purs"),
-    module_export_invalid => include_str!("parser/ModuleExportInvalid.purs"),
-    module_export_module => include_str!("parser/ModuleExportModule.purs"),
-    module_export_type_operator => include_str!("parser/ModuleExportTypeOperator.purs"),
-    module_export_type_operator_error => include_str!("parser/ModuleExportTypeOperatorError.purs"),
-    module_export_type_operator_error_end => include_str!("parser/ModuleExportTypeOperatorErrorEnd.purs"),
-    module_header => include_str!("parser/ModuleHeader.purs"),
-    module_header_prefixed => include_str!("parser/ModuleHeaderPrefixed.purs"),
-    module_import => include_str!("parser/ModuleImport.purs"),
-    module_import_error => include_str!("parser/ModuleImportError.purs"),
-    non_reserved => include_str!("parser/NonReserved.purs"),
-    types => include_str!("parser/Types.purs"),
-    type_parenthesized => include_str!("parser/TypeParenthesized.purs"),
-    type_record => include_str!("parser/TypeRecord.purs"),
-    value_annotation => include_str!("parser/ValueAnnotation.purs"),
-    value_equation => include_str!("parser/ValueEquation.purs"),
-    value_equation_recovery => include_str!("parser/ValueEquationRecovery.purs"),
-);
+test_each_file! { in "./crates/parsing/tests/parser" => |content: &str| {
+    let lexed = lexing::lex(content);
+    let tokens = lexing::layout(&lexed);
+    let (node, errors) = parsing::parse(&lexed, &tokens);
+    insta::assert_debug_snapshot!((node, errors));
+}}
