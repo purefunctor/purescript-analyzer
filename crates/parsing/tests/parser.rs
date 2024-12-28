458de0e2066e136171005d8ae91e9083cee7c03e
Justin Garcia <purefunctor@gmail.com>
2024-12-29 03:21:32 +0800

    Assert that parsing is lossless

--- crates/parsing/tests/parser.rs
+++ crates/parsing/tests/parser.rs
@@ -6,3 +6,10 @@ test_each_file! { in "./crates/parsing/tests/parser" => |content: &str| {
     let (node, errors) = parsing::parse(&lexed, &tokens);
     insta::assert_debug_snapshot!((node, errors));
 }}
+
+test_each_file! { in "./crates/parsing/tests/parser" as lossless => |content: &str| {
+    let lexed = lexing::lex(content);
+    let tokens = lexing::layout(&lexed);
+    let (node, _) = parsing::parse(&lexed, &tokens);
+    assert_eq!(node.to_string(), content);
+}}
