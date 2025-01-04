51b0dcc1d85380f3bfc7e5f58b7da82b371e22e7
Justin Garcia <purefunctor@gmail.com>
2025-01-04 23:43:14 +0800

    Fix fallback value

--- docs/svelte.config.js
+++ docs/svelte.config.js
@@ -12,7 +12,7 @@ const config = {
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter({
-      fallback: "/404.html",
+      fallback: "404.html",
     }),
     paths: {
       base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
