5a7e96aa83e51214fd88949b26317e866f929516
Justin Garcia <purefunctor@gmail.com>
2025-02-19 15:51:23 +0800

    Use la_arena in building

--- crates/building/src/files.rs
+++ crates/building/src/files.rs
@@ -1,8 +1,8 @@
 use core::str;
 use std::sync::Arc;
 
-use id::Id;
 use indexmap::IndexMap;
+use la_arena::{Idx, RawIdx};
 use rustc_hash::FxBuildHasher;
 
 #[derive(Default)]
@@ -12,29 +12,31 @@ pub struct Files {
 
 pub struct File;
 
-pub type FileId = Id<File>;
+pub type FileId = Idx<File>;
 
 impl Files {
     pub fn insert(&mut self, k: impl Into<Arc<str>>, v: impl Into<Arc<str>>) -> FileId {
         let k = k.into();
         let v = v.into();
         let (index, _) = self.files.insert_full(k, v);
-        Id::from_raw(index)
+        Idx::from_raw(RawIdx::from_u32(index as u32))
     }
 
     pub fn id(&self, k: &str) -> Option<FileId> {
-        self.files.get_full(k).map(|(index, _, _)| Id::from_raw(index))
+        self.files.get_full(k).map(|(index, _, _)| Idx::from_raw(RawIdx::from_u32(index as u32)))
     }
 
     pub fn path(&self, id: FileId) -> Arc<str> {
-        let index = id.into();
-        let (path, _) = self.files.get_index(index).expect("valid FileId");
+        let index = id.into_raw().into_u32() as usize;
+        let (path, _) =
+            self.files.get_index(index).expect("invariant violated: expected valid FileId");
         Arc::clone(path)
     }
 
     pub fn content(&self, id: FileId) -> Arc<str> {
-        let index = id.into();
-        let (_, contents) = self.files.get_index(index).expect("valid FileId");
+        let index = id.into_raw().into_u32() as usize;
+        let (_, contents) =
+            self.files.get_index(index).expect("invariant violated: expected valid FileId");
         Arc::clone(contents)
     }
 }
