eba348175aab994af4905de36afc174e418974f5
Justin Garcia <purefunctor@gmail.com>
2025-05-06 04:47:27 +0800

    Initial integration tests for resolution

--- crates/resolving/tests/resolving.rs
+++ crates/resolving/tests/resolving.rs
@@ -1,94 +1,79 @@
-use std::sync::Arc;
+use std::{fs, path::Path, sync::Arc};
 
 use files::FileId;
 use indexing::FullIndexedModule;
-use la_arena::RawIdx;
-use lowering::FullLoweredModule;
 use resolving::{External, FullResolvedModule};
 use rowan::ast::AstNode;
+use rustc_hash::FxHashMap;
+use smol_str::{SmolStr, SmolStrBuilder};
 use syntax::cst;
 
-pub const MAIN: &str = include_str!("fixtures/Main.txt");
-pub const LIB: &str = include_str!("fixtures/Lib.txt");
-pub const INTERNAL: &str = include_str!("fixtures/Internal.txt");
-
-macro_rules! file {
-    ($id:expr) => {
-        FileId::from_raw(RawIdx::from_u32($id))
-    };
+#[derive(Debug, Default)]
+struct IntegrationTestExternal {
+    files: files::Files,
+    names: FxHashMap<SmolStr, FileId>,
 }
 
-pub fn parse(id: FileId) -> cst::Module {
-    let id = id.into_raw().into_u32();
-    let source = match id {
-        0 => MAIN,
-        1 => LIB,
-        2 => INTERNAL,
-        _ => unreachable!(),
-    };
-
-    let lexed = lexing::lex(source);
-    let tokens = lexing::layout(&lexed);
-
-    let (module, _) = parsing::parse(&lexed, &tokens);
-
-    cst::Module::cast(module).unwrap()
-}
-
-struct TestExternal;
-
-impl TestExternal {
-    fn lowered(&mut self, id: FileId) -> Arc<FullLoweredModule> {
-        let module = parse(id);
-        let indexed = self.indexed(id);
-        let lowered = lowering::lower_module(&module, &indexed);
-        Arc::new(lowered)
+impl IntegrationTestExternal {
+    fn parsed(&mut self, id: FileId) -> Arc<cst::Module> {
+        let source = self.files.content(id);
+        let lexed = lexing::lex(&source);
+        let tokens = lexing::layout(&lexed);
+        let (module, _) = parsing::parse(&lexed, &tokens);
+        Arc::new(cst::Module::cast(module).unwrap())
     }
 }
 
-impl External for TestExternal {
+impl External for IntegrationTestExternal {
     fn indexed(&mut self, id: FileId) -> Arc<FullIndexedModule> {
-        let module = parse(id);
-        let index = indexing::index_module(&module);
-        Arc::new(index)
+        let cst = self.parsed(id);
+        Arc::new(indexing::index_module(&cst))
     }
 
     fn resolved(&mut self, id: FileId) -> Arc<FullResolvedModule> {
-        let exports = resolving::resolve_module(self, id);
-        Arc::new(exports)
+        Arc::new(resolving::resolve_module(self, id))
     }
 
     fn file_id(&mut self, name: &str) -> FileId {
-        match name {
-            "Main" => file!(0),
-            "Lib" => file!(1),
-            "Internal" => file!(2),
-            _ => unreachable!(),
-        }
+        *self.names.get(name).unwrap()
     }
 }
 
-#[test]
-fn test_basic() {
-    let mut external = TestExternal;
+fn create_external(folder: &str) -> IntegrationTestExternal {
+    let mut external = IntegrationTestExternal::default();
 
-    let lowered = external.lowered(file!(0));
-    let resolved = external.resolved(file!(0));
+    let manifest = Path::new(env!("CARGO_MANIFEST_DIR"));
+    let folder = manifest.join("tests/fixtures").join(folder);
 
-    for (id, deferred) in lowered.graph.deferred() {
-        match deferred.domain {
-            lowering::ResolutionDomain::Term => {
-                if let Some(name) = &deferred.name {
-                    let r = resolved.lookup_term(None, name);
-                    println!("{:?} = {:?}", id, r);
-                }
+    for entry in folder.read_dir().unwrap() {
+        let Ok(entry) = entry else { continue };
+        let path = entry.path();
+
+        let k = format!("file://{}", path.to_str().unwrap());
+        let v = fs::read_to_string(path).unwrap();
+
+        let id = external.files.insert(k, v);
+        let cst = external.parsed(id);
+
+        if let Some(cst) = cst.header().and_then(|cst| cst.name()) {
+            let mut builder = SmolStrBuilder::default();
+            if let Some(token) = cst.qualifier().and_then(|cst| cst.text()) {
+                builder.push_str(token.text());
             }
-            lowering::ResolutionDomain::Type => {
-                if let Some(name) = &deferred.name {
-                    let r = resolved.lookup_type(None, name);
-                    println!("{:?} = {:?}", id, r);
-                }
+            if let Some(token) = cst.name_token() {
+                builder.push_str(token.text());
             }
+            let name = builder.finish();
+            external.names.insert(name, id);
         }
     }
+
+    external
+}
+
+#[test]
+fn test_001_local_resolution() {
+    let mut external = create_external("001_local_resolution");
+    let id = external.file_id("Main");
+    insta::assert_debug_snapshot!(external.resolved(id));
 }
