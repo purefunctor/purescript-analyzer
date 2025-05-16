4c843705e9ededa8a56b3e9d6c6935e559f62ed0
Justin Garcia <purefunctor@gmail.com>
2025-05-16 14:10:36 +0800

    Make download-registry a build warning

--- tests-package-set/build.rs
+++ tests-package-set/build.rs
@@ -1,6 +1,6 @@
 fn main() {
     println!("cargo::rerun-if-changed=packages");
     if !std::fs::exists("packages").unwrap() {
-        println!("cargo::error=node download-registry.js");
+        println!("cargo::warning=node download-registry.js");
     }
 }
