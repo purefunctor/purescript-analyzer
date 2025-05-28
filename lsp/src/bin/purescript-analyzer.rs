f2dee7273a144c6d6d5fcb3b622fe5e2a83d5b34
Justin Garcia <purefunctor@gmail.com>
2025-05-28 14:12:21 +0800

    Start logging to temporary file instead

--- lsp/src/bin/purescript-analyzer.rs
+++ lsp/src/bin/purescript-analyzer.rs
@@ -1,11 +1,22 @@
+use std::fs::OpenOptions;
+
 use lsp::Backend;
 use tower_lsp::{LspService, Server};
+use tracing::Level;
 
 #[tokio::main]
 async fn main() {
+    let log_file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open("/tmp/purescript-analyzer.log")
+        .expect("Failed to open log file");
+
     let stdin = tokio::io::stdin();
     let stdout = tokio::io::stdout();
-    tracing_subscriber::fmt().init();
+
+    tracing_subscriber::fmt().with_max_level(Level::INFO).with_writer(log_file).init();
     let (service, socket) = LspService::new(Backend::new);
+
     Server::new(stdin, stdout, socket).serve(service).await;
 }
