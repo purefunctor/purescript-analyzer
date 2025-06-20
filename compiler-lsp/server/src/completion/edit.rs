252f760d56fa4d6af532b6dc54765d3b071b3daa
Justin Garcia <purefunctor@gmail.com>
2025-06-21 00:44:30 +0800

    Make generic import_item function

--- compiler-lsp/server/src/completion/edit.rs
+++ compiler-lsp/server/src/completion/edit.rs
@@ -4,32 +4,33 @@ use async_lsp::lsp_types::Range;
 use files::FileId;
 use indexing::{FullIndexedModule, ImportKind, TermItemId, TermItemKind, TypeItemId, TypeItemKind};
 use itertools::Itertools;
+use resolving::ResolvedImport;
 use rowan::ast::AstNode;
 use smol_str::{SmolStrBuilder, ToSmolStr};
 
 use crate::{State, completion::Context, locate};
 
-pub(super) fn term_import_item(
+fn import_item<F, G>(
     state: &mut State,
     context: &Context,
     module_name: &str,
-    term_name: &str,
     file_id: FileId,
-    term_id: TermItemId,
-) -> (Option<String>, Option<Range>) {
-    let import_containing_term =
-        context.resolved.unqualified.iter().find_map(|import| {
-            match import.lookup_term(term_name) {
-                Some((f_id, t_id, kind)) if (f_id, t_id) == (file_id, term_id) => {
-                    Some((import, kind))
-                }
-                _ => None,
-            }
-        });
+    lookup_fn: F,
+    name_fn: G,
+) -> (Option<String>, Option<Range>)
+where
+    F: Fn(&ResolvedImport) -> Option<ImportKind>,
+    G: Fn(&FullIndexedModule) -> Option<String>,
+{
+    let import_containing_item = context
+        .resolved
+        .unqualified
+        .iter()
+        .find_map(|import| lookup_fn(import).map(|kind| (import, kind)));
 
     let import_indexed = state.runtime.indexed(file_id);
 
-    if let Some((import, kind)) = import_containing_term {
+    if let Some((import, kind)) = import_containing_item {
         // Implicit or hidden imports are skipped.
         if !matches!(import.kind, ImportKind::Explicit) {
             return (None, None);
@@ -47,7 +48,7 @@ pub(super) fn term_import_item(
         let import_list =
             node.import_list().expect("invariant violated: ImportKind::Explicit w/ no ImportList");
 
-        let Some(import_item_name) = term_import_name(&import_indexed, term_name, term_id) else {
+        let Some(import_item_name) = name_fn(&import_indexed) else {
             return (None, None);
         };
 
@@ -76,13 +77,35 @@ pub(super) fn term_import_item(
 
         (Some(import_text), import_range)
     } else {
-        if let Some(import_item_name) = term_import_name(&import_indexed, term_name, term_id) {
+        if let Some(import_item_name) = name_fn(&import_indexed) {
             let import_text = format!("import {module_name} ({import_item_name})\n");
             (Some(import_text), None)
         } else {
             (None, None)
         }
     }
+}
+
+pub(super) fn term_import_item(
+    state: &mut State,
+    context: &Context,
+    module_name: &str,
+    term_name: &str,
+    file_id: FileId,
+    term_id: TermItemId,
+) -> (Option<String>, Option<Range>) {
+    import_item(
+        state,
+        context,
+        module_name,
+        file_id,
+        |import| {
+            import
+                .lookup_term(term_name)
+                .and_then(|(f, t, k)| if (f, t) == (file_id, term_id) { Some(k) } else { None })
+        },
+        |import_indexed| term_import_name(import_indexed, term_name, term_id),
+    )
 }
 
 pub(super) fn type_import_item(
@@ -93,72 +116,18 @@ pub(super) fn type_import_item(
     file_id: FileId,
     type_id: TypeItemId,
 ) -> (Option<String>, Option<Range>) {
-    let import_containing_type =
-        context.resolved.unqualified.iter().find_map(|import| {
-            match import.lookup_type(type_name) {
-                Some((f_id, t_id, kind)) if (f_id, t_id) == (file_id, type_id) => {
-                    Some((import, kind))
-                }
-                _ => None,
-            }
-        });
-
-    let import_indexed = state.runtime.indexed(file_id);
-
-    if let Some((import, kind)) = import_containing_type {
-        // Implicit or hidden imports are skipped.
-        if !matches!(import.kind, ImportKind::Explicit) {
-            return (None, None);
-        }
-
-        // Names that are already imported are skipped.
-        if matches!(kind, ImportKind::Explicit) {
-            return (None, None);
-        }
-
-        let ptr = &context.indexed.source[import.id];
-        let root = context.parsed.syntax_node();
-        let node = ptr.to_node(&root);
-
-        let import_list =
-            node.import_list().expect("invariant violated: ImportKind::Explicit w/ no ImportList");
-
-        let Some(import_item_name) = type_import_name(&import_indexed, type_name, type_id) else {
-            return (None, None);
-        };
-
-        let import_item_name = {
-            let mut buffer = SmolStrBuilder::new();
-            buffer.push(' ');
-            buffer.push_str(&import_item_name);
-            buffer.finish()
-        };
-
-        let import_list = import_list
-            .children()
-            .map(|cst| {
-                let cst = cst.syntax();
-                cst.text().to_smolstr()
-            })
-            .chain(iter::once(import_item_name))
-            .join(",");
-
-        let import_text = format!("import {module_name} ({import_list})");
-
-        let import_range = {
-            let text_range = locate::text_range_after_annotation(&ptr.syntax_node_ptr(), &root);
-            text_range.map(|range| locate::text_range_to_range(context.content, range))
-        };
-
-        (Some(import_text), import_range)
-    } else {
-        if let Some(import_item_name) = type_import_name(&import_indexed, type_name, type_id) {
-            let import_text = format!("import {module_name} ({import_item_name})\n");
-            (Some(import_text), None)
-        } else {
-            (None, None)
-        }
-    }
+    import_item(
+        state,
+        context,
+        module_name,
+        file_id,
+        |import| {
+            import
+                .lookup_type(type_name)
+                .and_then(|(f, t, k)| if (f, t) == (file_id, type_id) { Some(k) } else { None })
+        },
+        |import_indexed| type_import_name(import_indexed, type_name, type_id),
+    )
 }
 
 fn term_import_name(
