97ce3ac76aae4d4a40da041b1ae10f3fbf4e625b
Justin Garcia <purefunctor@gmail.com>
2025-08-25 02:06:53 +0800

    Move lowering tests into integration tests

--- tests-integration/src/core.rs
+++ tests-integration/src/core.rs
@@ -3,6 +3,11 @@ use std::fmt::Write;
 use analyzer::QueryEngine;
 use files::FileId;
 use indexing::ImportKind;
+use lowering::{
+    ExpressionKind, GraphNode, ImplicitTypeVariable, LetBound, Nominal, TermVariableResolution,
+    TypeKind, TypeVariableResolution,
+};
+use rowan::ast::AstNode;
 
 pub fn report_resolved(engine: &QueryEngine, id: FileId, name: &str) -> String {
     let resolved = engine.resolved(id).unwrap();
@@ -82,6 +87,113 @@ pub fn report_resolved(engine: &QueryEngine, id: FileId, name: &str) -> String {
     writeln!(buffer, "Errors:").unwrap();
     for error in &resolved.errors {
         writeln!(buffer, "  - {error:?}").unwrap();
+    }
+
+    buffer
+}
+
+pub fn report_lowered(engine: &QueryEngine, id: FileId, name: &str) -> String {
+    let (parsed, _) = engine.parsed(id).unwrap();
+    let lowered = engine.lowered(id).unwrap();
+
+    let module = parsed.cst();
+    let intermediate = &lowered.intermediate;
+    let source = &lowered.source;
+    let graph = &lowered.graph;
+
+    let mut buffer = String::default();
+    writeln!(buffer, "module {name}").unwrap();
+
+    writeln!(buffer).unwrap();
+    writeln!(buffer, "Expressions:").unwrap();
+    writeln!(buffer).unwrap();
+    for (expression_id, _) in intermediate.iter_expression() {
+        let Some(ExpressionKind::Variable { resolution, .. }) =
+            intermediate.index_expression_kind(expression_id)
+        else {
+            continue;
+        };
+
+        let cst = &source[expression_id];
+        let root = module.syntax();
+
+        let node = cst.syntax_node_ptr().to_node(root);
+        let text = node.text().to_string();
+        let range = node.text_range();
+
+        writeln!(buffer, "{}@{:?}", text.trim(), range).unwrap();
+        if let Some(resolution) = resolution {
+            match resolution {
+                TermVariableResolution::Global => {
+                    writeln!(buffer, "  resolves to top-level name").unwrap();
+                }
+                TermVariableResolution::Binder(binder) => {
+                    let cst = &source[*binder];
+                    let range = cst.syntax_node_ptr().text_range();
+                    writeln!(buffer, "  resolves to binder {range:?}").unwrap();
+                }
+                TermVariableResolution::Let(LetBound { signature, equations }) => {
+                    if let Some(signature) = signature {
+                        let cst = &source[*signature];
+                        let range = cst.syntax_node_ptr().text_range();
+                        writeln!(buffer, "  resolves to signature {range:?}").unwrap();
+                    }
+                    for equation in equations.iter() {
+                        let cst = &source[*equation];
+                        let range = cst.syntax_node_ptr().text_range();
+                        writeln!(buffer, "  resolves to equation {range:?}").unwrap();
+                    }
+                }
+                TermVariableResolution::Nominal(Nominal { .. }) => {
+                    writeln!(buffer, "  resolves to top-level name").unwrap();
+                }
+            }
+        } else {
+            writeln!(buffer, "  resolves to nothing").unwrap();
+        }
+    }
+
+    writeln!(buffer, "\nTypes:\n").unwrap();
+
+    for (type_id, _) in intermediate.iter_type() {
+        let Some(TypeKind::Variable { resolution, .. }) = intermediate.index_type_kind(type_id)
+        else {
+            continue;
+        };
+        let cst = &source[type_id];
+        let range = cst.syntax_node_ptr().text_range();
+        writeln!(buffer, "{range:?}").unwrap();
+        if let Some(resolution) = resolution {
+            match resolution {
+                TypeVariableResolution::Forall(id) => {
+                    let cst = &source[*id];
+                    let range = cst.syntax_node_ptr().text_range();
+                    writeln!(buffer, "  resolves to forall {range:?}").unwrap();
+                }
+                TypeVariableResolution::Implicit(ImplicitTypeVariable { binding, node, id }) => {
+                    if let GraphNode::Implicit { bindings, .. } = &graph[*node] {
+                        let (name, type_ids) =
+                            bindings.get_index(*id).expect("invariant violated: invalid index");
+                        if *binding {
+                            writeln!(buffer, "  introduces a constraint variable {name:?}")
+                                .unwrap();
+                        } else {
+                            writeln!(buffer, "  resolves to a constraint variable {name:?}")
+                                .unwrap();
+                            for &type_id in type_ids {
+                                let cst = &source[type_id];
+                                let range = cst.syntax_node_ptr().text_range();
+                                writeln!(buffer, "    {range:?}").unwrap();
+                            }
+                        }
+                    } else {
+                        writeln!(buffer, "  did not resolve to constraint variable!").unwrap();
+                    }
+                }
+            }
+        } else {
+            writeln!(buffer, "  resolves to nothing").unwrap();
+        }
     }
 
     buffer
