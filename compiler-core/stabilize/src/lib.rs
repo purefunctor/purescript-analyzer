d1649aa8355dd22cf03631d05e23d70b52a9b42f
Justin Garcia <purefunctor@gmail.com>
2025-09-22 00:49:59 +0800

    Small documentation blurb

--- compiler-core/stabilize/src/lib.rs
+++ compiler-core/stabilize/src/lib.rs
@@ -7,6 +7,18 @@ pub use map::*;
 use rowan::WalkEvent;
 use syntax::{SyntaxKind, SyntaxNode};
 
+/// Allocates stable IDs for text ranges.
+///
+/// Text ranges are often used to annotate syntax nodes with positional
+/// information; however, they're ill-suited for query-based compilers
+/// designed to cache semantic information.
+///
+/// This algorithm allocates stable and type-safe IDs that can be used
+/// for locating syntax nodes and associating information to text ranges.
+///
+/// A separate stabilization pass enables better caching behaviour for
+/// algorithms like indexing and lowering, which used to have their own
+/// stabilization passes as they traversed the CST.
 pub fn stabilized(node: &SyntaxNode) -> StabilizedModule {
     let mut ast_ptr_map = StabilizedModule::default();
 
