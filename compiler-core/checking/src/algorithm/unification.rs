8d18f3dd48ef59aa3ae9ae0ffcbc3089b0e83868
Justin Garcia <purefunctor@gmail.com>
2025-11-26 00:03:57 +0800

    Use type ID equality in unification

--- compiler-core/checking/src/algorithm/unification.rs
+++ compiler-core/checking/src/algorithm/unification.rs
@@ -18,6 +18,10 @@ where
     let t1 = state.normalize_type(t1);
     let t2 = state.normalize_type(t2);
 
+    if t1 == t2 {
+        return true;
+    }
+
     let t1_core = state.storage[t1].clone();
     let t2_core = state.storage[t2].clone();
 
@@ -54,10 +58,17 @@ where
     let t1 = state.normalize_type(t1);
     let t2 = state.normalize_type(t2);
 
-    let unifies = match (&state.storage[t1], &state.storage[t2]) {
+    if t1 == t2 {
+        return true;
+    }
+
+    let t1_core = state.storage[t1].clone();
+    let t2_core = state.storage[t2].clone();
+
+    let unifies = match (t1_core, t2_core) {
         (
-            &Type::Application(t1_function, t1_argument),
-            &Type::Application(t2_function, t2_argument),
+            Type::Application(t1_function, t1_argument),
+            Type::Application(t2_function, t2_argument),
         ) => {
             unify(state, context, t1_function, t2_function)
                 && unify(state, context, t1_argument, t2_argument)
@@ -67,27 +78,26 @@ where
             (t1_file_id, t1_item_id) == (t2_file_id, t2_item_id)
         }
 
-        (&Type::Function(t1_argument, t1_result), &Type::Function(t2_argument, t2_result)) => {
+        (Type::Function(t1_argument, t1_result), Type::Function(t2_argument, t2_result)) => {
             unify(state, context, t1_argument, t2_argument)
                 && unify(state, context, t1_result, t2_result)
         }
 
-        (&Type::Unification(unification_id), _) => {
+        (Type::Unification(unification_id), _) => {
             solve(state, context, unification_id, t2).is_some()
         }
 
-        (_, &Type::Unification(unification_id)) => {
+        (_, Type::Unification(unification_id)) => {
             solve(state, context, unification_id, t1).is_some()
         }
 
-        (
-            &Type::Variable(Variable::Bound(t1_index)),
-            &Type::Variable(Variable::Bound(t2_index)),
-        ) => t1_index == t2_index,
+        (Type::Variable(Variable::Bound(t1_index)), Type::Variable(Variable::Bound(t2_index))) => {
+            t1_index == t2_index
+        }
 
         (
-            &Type::Variable(Variable::Skolem(t1_level, _)),
-            &Type::Variable(Variable::Skolem(t2_level, _)),
+            Type::Variable(Variable::Skolem(t1_level, _)),
+            Type::Variable(Variable::Skolem(t2_level, _)),
         ) => t1_level == t2_level,
 
         _ => false,
@@ -114,7 +124,7 @@ where
     Q: ExternalQueries,
 {
     let codomain = state.bound.size();
-    let occurs = Some(unification_id);
+    let occurs = unification_id;
 
     if !promote_type(state, occurs, codomain, unification_id, solution) {
         return None;
@@ -131,7 +141,7 @@ where
 
 pub fn promote_type(
     state: &mut CheckState,
-    occurs: Option<u32>,
+    occurs: u32,
     codomain: debruijn::Size,
     unification_id: u32,
     solution: TypeId,
@@ -165,7 +175,7 @@ pub fn promote_type(
             let unification = state.unification.get(unification_id);
             let solution = state.unification.get(solution_id);
 
-            if occurs == Some(solution_id) {
+            if occurs == solution_id {
                 return false;
             }
 
