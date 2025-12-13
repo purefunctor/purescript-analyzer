ea49af2f2bdf16ef482331dbade7f493a128d781
Justin Garcia <purefunctor@gmail.com>
2025-12-13 18:29:53 +0800

    Add reporting for type synonym cycles

--- compiler-bin/src/lsp/event.rs
+++ compiler-bin/src/lsp/event.rs
@@ -2,11 +2,26 @@ use analyzer::{common, locate};
 use async_lsp::LanguageClient;
 use async_lsp::lsp_types::*;
 use files::FileId;
+use indexing::IndexedModule;
+use indexing::TypeItemKind;
+use lowering::{LoweringError, RecursiveSynonym};
+use resolving::{ResolvedModule, ResolvingError};
 use rowan::ast::AstNode;
+use stabilizing::StabilizedModule;
 use syntax::SyntaxNode;
 
 use crate::lsp::error::LspError;
 use crate::lsp::{State, StateSnapshot};
+
+struct DiagnosticsContext<'a> {
+    uri: &'a Url,
+    content: &'a str,
+    root: &'a SyntaxNode,
+    stabilized: &'a StabilizedModule,
+    indexed: &'a IndexedModule,
+    resolved: &'a ResolvedModule,
+    lowered: &'a lowering::LoweredModule,
+}
 
 pub fn emit_collect_diagnostics(state: &mut State, uri: Url) -> Result<(), LspError> {
     let files = state.files.read();
@@ -39,17 +54,28 @@ fn collect_diagnostics_core(
     let root = parsed.syntax_node();
 
     let stabilized = snapshot.engine.stabilized(id)?;
+    let indexed = snapshot.engine.indexed(id)?;
     let resolved = snapshot.engine.resolved(id)?;
     let lowered = snapshot.engine.lowered(id)?;
-
-    let mut diagnostics = vec![];
-    diagnostics.extend(resolved_diagnostics(&content, &root, &stabilized, &resolved));
-    diagnostics.extend(lowered_diagnostics(&content, &root, &stabilized, &lowered));
 
     let uri = {
         let files = snapshot.files.read();
         common::file_uri(&snapshot.engine, &files, id)?
     };
+
+    let context = DiagnosticsContext {
+        uri: &uri,
+        content: &content,
+        root: &root,
+        stabilized: &stabilized,
+        indexed: &indexed,
+        resolved: &resolved,
+        lowered: &lowered,
+    };
+
+    let mut diagnostics = vec![];
+    diagnostics.extend(resolved_diagnostics(&context));
+    diagnostics.extend(lowered_diagnostics(&context));
 
     snapshot.client.publish_diagnostics(PublishDiagnosticsParams {
         uri,
@@ -60,61 +86,65 @@ fn collect_diagnostics_core(
     Ok(())
 }
 
-fn lowered_diagnostics<'context>(
-    content: &'context str,
-    root: &'context SyntaxNode,
-    stabilized: &'context stabilizing::StabilizedModule,
-    lowered: &'context lowering::LoweredModule,
-) -> impl Iterator<Item = Diagnostic> + 'context {
-    lowered.errors.iter().filter_map(|error| lowered_error(content, root, stabilized, error))
+fn lowered_diagnostics<'a>(
+    context: &'a DiagnosticsContext<'a>,
+) -> impl Iterator<Item = Diagnostic> + 'a {
+    context.lowered.errors.iter().filter_map(|error| lowered_error(context, error))
 }
 
-fn lowered_error<'context>(
-    content: &'context str,
-    root: &'context SyntaxNode,
-    stabilized: &'context stabilizing::StabilizedModule,
-    error: &lowering::LoweringError,
-) -> Option<Diagnostic> {
+fn lowered_error(context: &DiagnosticsContext<'_>, error: &LoweringError) -> Option<Diagnostic> {
     match error {
-        lowering::LoweringError::NotInScope(not_in_scope) => {
+        LoweringError::NotInScope(not_in_scope) => {
             let (ptr, name) = match not_in_scope {
-                lowering::NotInScope::ExprConstructor { id } => (stabilized.syntax_ptr(*id)?, None),
-                lowering::NotInScope::ExprVariable { id } => (stabilized.syntax_ptr(*id)?, None),
-                lowering::NotInScope::ExprOperatorName { id } => {
-                    (stabilized.syntax_ptr(*id)?, None)
+                lowering::NotInScope::ExprConstructor { id } => {
+                    (context.stabilized.syntax_ptr(*id)?, None)
                 }
-                lowering::NotInScope::TypeConstructor { id } => (stabilized.syntax_ptr(*id)?, None),
-                lowering::NotInScope::TypeVariable { id } => (stabilized.syntax_ptr(*id)?, None),
+                lowering::NotInScope::ExprVariable { id } => {
+                    (context.stabilized.syntax_ptr(*id)?, None)
+                }
+                lowering::NotInScope::ExprOperatorName { id } => {
+                    (context.stabilized.syntax_ptr(*id)?, None)
+                }
+                lowering::NotInScope::TypeConstructor { id } => {
+                    (context.stabilized.syntax_ptr(*id)?, None)
+                }
+                lowering::NotInScope::TypeVariable { id } => {
+                    (context.stabilized.syntax_ptr(*id)?, None)
+                }
                 lowering::NotInScope::TypeOperatorName { id } => {
-                    (stabilized.syntax_ptr(*id)?, None)
+                    (context.stabilized.syntax_ptr(*id)?, None)
                 }
                 lowering::NotInScope::DoFn { kind, id } => (
-                    stabilized.syntax_ptr(*id)?,
+                    context.stabilized.syntax_ptr(*id)?,
                     match kind {
                         lowering::DoFn::Bind => Some("bind"),
                         lowering::DoFn::Discard => Some("discard"),
                     },
                 ),
                 lowering::NotInScope::AdoFn { kind, id } => (
-                    stabilized.syntax_ptr(*id)?,
+                    context.stabilized.syntax_ptr(*id)?,
                     match kind {
                         lowering::AdoFn::Map => Some("map"),
                         lowering::AdoFn::Apply => Some("apply"),
                     },
                 ),
-                lowering::NotInScope::TermOperator { id } => (stabilized.syntax_ptr(*id)?, None),
-                lowering::NotInScope::TypeOperator { id } => (stabilized.syntax_ptr(*id)?, None),
+                lowering::NotInScope::TermOperator { id } => {
+                    (context.stabilized.syntax_ptr(*id)?, None)
+                }
+                lowering::NotInScope::TypeOperator { id } => {
+                    (context.stabilized.syntax_ptr(*id)?, None)
+                }
             };
 
             let message = if let Some(name) = name {
                 format!("'{name}' is not in scope")
             } else {
-                let range = ptr.to_node(root).text_range();
-                let name = content[range].trim();
+                let range = ptr.to_node(context.root).text_range();
+                let name = context.content[range].trim();
                 format!("'{name}' is not in scope")
             };
 
-            let range = locate::syntax_range(content, root, &ptr)?;
+            let range = locate::syntax_range(context.content, context.root, &ptr)?;
 
             Some(Diagnostic {
                 range,
@@ -128,47 +158,80 @@ fn lowered_error<'context>(
                 data: None,
             })
         }
+
+        LoweringError::RecursiveSynonym(RecursiveSynonym { group }) => {
+            let equations = group.iter().filter_map(|id| {
+                if let TypeItemKind::Synonym { equation, .. } = context.indexed.items[*id].kind {
+                    equation
+                } else {
+                    None
+                }
+            });
+
+            let locations = equations.filter_map(|equation| {
+                let syntax_ptr = context.stabilized.syntax_ptr(equation)?;
+                locate::syntax_range(context.content, context.root, &syntax_ptr)
+            });
+
+            let locations: Vec<_> = locations.collect();
+            let [range, associated @ ..] = &locations[..] else { return None };
+
+            let related_information = associated.iter().map(|&range| {
+                let uri = context.uri.clone();
+                let location = Location { uri, range };
+                DiagnosticRelatedInformation {
+                    location,
+                    message: "Includes this type synonym".to_string(),
+                }
+            });
+
+            let related_information = related_information.collect();
+
+            Some(Diagnostic {
+                range: *range,
+                severity: Some(DiagnosticSeverity::ERROR),
+                code: Some(NumberOrString::String("RecursiveSynonym".to_string())),
+                code_description: None,
+                source: Some("analyzer/lowering".to_string()),
+                message: "Invalid type synonym cycle".to_string(),
+                related_information: Some(related_information),
+                tags: None,
+                data: None,
+            })
+        }
     }
 }
 
-fn resolved_diagnostics<'context>(
-    content: &'context str,
-    root: &'context SyntaxNode,
-    stabilized: &'context stabilizing::StabilizedModule,
-    resolved: &'context resolving::ResolvedModule,
-) -> impl Iterator<Item = Diagnostic> + 'context {
-    resolved.errors.iter().filter_map(|error| resolved_error(content, root, stabilized, error))
+fn resolved_diagnostics<'a>(
+    context: &'a DiagnosticsContext<'a>,
+) -> impl Iterator<Item = Diagnostic> + 'a {
+    context.resolved.errors.iter().filter_map(|error| resolved_error(context, error))
 }
 
-fn resolved_error<'context>(
-    content: &'context str,
-    root: &'context SyntaxNode,
-    stabilized: &'context stabilizing::StabilizedModule,
-    error: &'context resolving::ResolvingError,
-) -> Option<Diagnostic> {
+fn resolved_error(context: &DiagnosticsContext<'_>, error: &ResolvingError) -> Option<Diagnostic> {
     let source = Some("analyzer/resolving".to_string());
     match error {
-        resolving::ResolvingError::TermImportConflict { .. } => None,
+        ResolvingError::TermImportConflict { .. } => None,
 
-        resolving::ResolvingError::TypeImportConflict { .. } => None,
+        ResolvingError::TypeImportConflict { .. } => None,
 
-        resolving::ResolvingError::TermExportConflict { .. } => None,
+        ResolvingError::TermExportConflict { .. } => None,
 
-        resolving::ResolvingError::TypeExportConflict { .. } => None,
+        ResolvingError::TypeExportConflict { .. } => None,
 
-        resolving::ResolvingError::ExistingTerm { .. } => None,
+        ResolvingError::ExistingTerm { .. } => None,
 
-        resolving::ResolvingError::ExistingType { .. } => None,
+        ResolvingError::ExistingType { .. } => None,
 
-        resolving::ResolvingError::InvalidImportStatement { id } => {
-            let ptr = stabilized.ast_ptr(*id)?;
+        ResolvingError::InvalidImportStatement { id } => {
+            let ptr = context.stabilized.ast_ptr(*id)?;
 
             let message = {
-                let cst = ptr.to_node(root);
+                let cst = ptr.to_node(context.root);
 
                 let name = cst.module_name().map(|cst| {
                     let range = cst.syntax().text_range();
-                    content[range].trim()
+                    context.content[range].trim()
                 });
 
                 let name = name.unwrap_or("<ParseError>");
@@ -176,7 +239,7 @@ fn resolved_error<'context>(
             };
 
             let ptr = ptr.syntax_node_ptr();
-            let range = locate::syntax_range(content, root, &ptr)?;
+            let range = locate::syntax_range(context.content, context.root, &ptr)?;
 
             Some(Diagnostic {
                 range,
@@ -191,16 +254,16 @@ fn resolved_error<'context>(
             })
         }
 
-        resolving::ResolvingError::InvalidImportItem { id } => {
-            let ptr = stabilized.syntax_ptr(*id)?;
+        ResolvingError::InvalidImportItem { id } => {
+            let ptr = context.stabilized.syntax_ptr(*id)?;
 
             let message = {
-                let range = ptr.to_node(root).text_range();
-                let name = content[range].trim();
+                let range = ptr.to_node(context.root).text_range();
+                let name = context.content[range].trim();
                 format!("Cannot import item '{name}'")
             };
 
-            let range = locate::syntax_range(content, root, &ptr)?;
+            let range = locate::syntax_range(context.content, context.root, &ptr)?;
 
             Some(Diagnostic {
                 range,
