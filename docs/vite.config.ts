19b0afb28b8e7e2d51d3c8ac6ec57a35d79e2ca1
Justin Garcia <git@purefunctor.me>
2026-01-02 17:06:48 +0800

    Add missing base for GH Pages

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/purescript-analyzer/",
   plugins: [react(), tailwindcss()],
   build: {
     outDir: "build",
