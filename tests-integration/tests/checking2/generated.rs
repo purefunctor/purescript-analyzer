ec12f30513c5a8f8d3248bf1df05dd90fa38786f
Justin Garcia <git@purefunctor.me>
2026-03-25 19:12:49 +0800

    Add failing test case for application-based function subtyping

--- tests-integration/tests/checking2/generated.rs
+++ tests-integration/tests/checking2/generated.rs
@@ -543,3 +543,5 @@ fn run_test(folder: &str, file: &str) {
 #[rustfmt::skip] #[test] fn test_258_self_recursive_through_operator_main() { run_test("258_self_recursive_through_operator", "Main"); }
 
 #[rustfmt::skip] #[test] fn test_259_hidden_forall_scope_check_main() { run_test("259_hidden_forall_scope_check", "Main"); }
+
+#[rustfmt::skip] #[test] fn test_260_hidden_forall_compose_application_subtype_main() { run_test("260_hidden_forall_compose_application_subtype", "Main"); }
