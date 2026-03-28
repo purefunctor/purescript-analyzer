221f7e0d013c0ce5fefb5296e32e0a10e654dde9
Justin Garcia <git@purefunctor.me>
2026-03-28 10:47:54 +0800

    Add failing test case for false given matching

--- tests-integration/tests/checking2/generated.rs
+++ tests-integration/tests/checking2/generated.rs
@@ -547,3 +547,5 @@ fn run_test(folder: &str, file: &str) {
 #[rustfmt::skip] #[test] fn test_260_hidden_forall_compose_application_subtype_main() { run_test("260_hidden_forall_compose_application_subtype", "Main"); }
 
 #[rustfmt::skip] #[test] fn test_261_instance_chain_stuck_matching_main() { run_test("261_instance_chain_stuck_matching", "Main"); }
+
+#[rustfmt::skip] #[test] fn test_262_given_stuck_match_collapse_main() { run_test("262_given_stuck_match_collapse", "Main"); }
